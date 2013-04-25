8c8c8c9b8488bfb82026590aef81b1d1ad1c42fb
Alexey Alekhin <laughedelic@gmail.com>
2013-04-25 17:20:23 +0200

    Version 0.6.1: Fixed empty g8 options

--- src/main/scala/gener8bundle.scala
+++ src/main/scala/gener8bundle.scala
@@ -35,7 +35,7 @@ case class BundleDescription(
   import giter8.G8._
 
   def dependencies_sbt(l: List[BundleDependency]): String = 
-    if (l.isEmpty) " "
+    if (l.isEmpty) ""
     else l.map(_.forSbt).mkString("libraryDependencies ++= Seq(", ", ", ")")
 
   def dependencies_class(l: List[BundleDependency]): String = 
@@ -45,16 +45,17 @@ case class BundleDescription(
   def toSeq: Seq[String] = {
     def format(k: String, v: String) = "--" + k + "=" + v.toString.replaceAll(" ", "\\ ")
     def opt[A](k: String, v: Option[A]) = v.toList.map((k, _))
+    def notEmpty(k: String, v: String) = if (v.isEmpty) Seq() else Seq((k, v))
 
     (Seq(("name", name)) ++ 
      opt("bundle_version", bundle_version) ++
      opt("description", description) ++
      opt("org", org) ++
      opt("scala_version", scala_version) ++
-     Seq( ("dependencies_sbt", dependencies_sbt(dependencies)),
-          ("dependencies_class", dependencies_class(dependencies)),
-          ("tool_version_sbt", ToolVersion(tool_version).forSbt),
-          ("tool_version_class", ToolVersion(tool_version).forClass))
+     notEmpty("dependencies_sbt", dependencies_sbt(dependencies)) ++
+     notEmpty("dependencies_class", dependencies_class(dependencies)) ++
+     notEmpty("tool_version_sbt", ToolVersion(tool_version).forSbt) ++
+     notEmpty("tool_version_class", ToolVersion(tool_version).forClass)
     ) map {case (k,v) => format(k,v)}
   }
 }
